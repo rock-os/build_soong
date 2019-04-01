4d4d7cef186fb4d7cb79d3283a82aa9b5f044d92
ShantanuSingh555 <singhshantanu555@gmail.com>
2019-04-01 13:57:24 +0530

    ROCKOS-IFY

--- android/variable.go
+++ android/variable.go
@@ -20,7 +20,7 @@ import (
 	"runtime"
 	"strings"
 
-	"gzosp/soong/android"
+	"rockos/soong/android"
 
 	"github.com/google/blueprint/proptools"
 )
@@ -117,8 +117,8 @@ type variableProperties struct {
 			Srcs         []string
 		}
 
-		// include Gzosp variables
-		Gzosp android.Product_variables
+		// include Rockos variables
+		Rockos android.Product_variables
 	} `android:"arch_variant"`
 }
 
@@ -237,8 +237,8 @@ type productVariables struct {
 
 	VendorVars map[string]map[string]string `json:",omitempty"`
 
-	// include Gzosp variables
-	Gzosp android.ProductVariables
+	// include Rockos variables
+	Rockos android.ProductVariables
 }
 
 func boolPtr(v bool) *bool {
